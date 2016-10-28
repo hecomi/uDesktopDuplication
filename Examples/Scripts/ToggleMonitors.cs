67348f83a0182b9dc5be61d1ab31c70907059db3
hecomi <info@hecomi.com>
2016-10-28 19:33:37 +0900

    add toggle example.

---
+++ Examples/Scripts/ToggleMonitors.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ToggleMonitors : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            var texture = GetComponent<uDesktopDuplication.Texture>();
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+                texture.monitorId--;
+            } else {
+                texture.monitorId++;
+            }
+        }
+    }
+}
+
