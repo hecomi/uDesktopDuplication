8aaf3cf74d9001a960288b6817ff154253df1597
hecomi <info@hecomi.com>
2016-10-28 17:28:22 +0900

    implement multiple screen.

---
+++ Scripts/uDD_Monitor.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace uDesktopDuplication
+{
+
+public class uDD_Monitor
+{
+    public uDD_Monitor(int id)
+    {
+        this.id = id;
+    }
+
+    public static int count
+    {
+        get { return uDD_Lib.GetMonitorCount(); }
+    }
+
+    public int id 
+    { 
+        get; 
+        private set; 
+    }
+
+    public string name
+    { 
+        get { return uDD_Lib.GetName(id); }
+    }
+
+    public bool isPrimary
+    { 
+        get { return uDD_Lib.IsPrimary(id); }
+    }
+
+    public int width 
+    { 
+        get { return uDD_Lib.GetWidth(id); }
+    }
+
+    public int height
+    { 
+        get { return uDD_Lib.GetHeight(id); }
+    }
+
+    public bool isPointerVisible
+    { 
+        get { return uDD_Lib.IsPointerVisible(id); }
+    }
+
+    public int pointerX
+    { 
+        get { return uDD_Lib.GetPointerX(id); }
+    }
+
+    public int pointerY
+    { 
+        get { return uDD_Lib.GetPointerY(id); }
+    }
+
+    public bool shouldBeUpdated
+    {
+        get; 
+        set;
+    }
+
+    private Texture2D texture_;
+    public Texture2D texture 
+    {
+        get 
+        { 
+            if (texture_ == null) {
+                texture_ = new Texture2D(width, height, TextureFormat.BGRA32, false);
+                uDD_Lib.SetTexturePtr(id, texture_.GetNativeTexturePtr());
+            }
+            return texture_;
+        }
+    }
+
+    public void Render()
+    {
+        GL.IssuePluginEvent(uDD_Lib.GetRenderEventFunc(), id);
+    }
+}
+
+}
---
+++ Scripts/uDD_Texture.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace uDesktopDuplication
+{
+
+public class uDD_Texture : MonoBehaviour
+{
+    public uDD_Monitor monitor { get; set; }
+
+    public bool invertX = false;
+    public bool invertY = false;
+
+    private Material material_;
+
+    void OnEnable()
+    {
+        if (monitor == null) {
+            monitor = uDD_Manager.primary;
+        }
+        material_ = GetComponent<Renderer>().material;
+        material_.mainTexture = monitor.texture;
+    }
+
+    void Update()
+    {
+        monitor.shouldBeUpdated = true;
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        if (invertX) {
+            material_.EnableKeyword("INVERT_X");
+        } else {
+            material_.DisableKeyword("INVERT_X");
+        }
+
+        if (invertY) {
+            material_.EnableKeyword("INVERT_Y");
+        } else {
+            material_.DisableKeyword("INVERT_Y");
+        }
+    }
+}
+
+}
