941bb2da918418cb5d09d7ef5896ba445a2fd4c9
hecomi <info@hecomi.com>
2016-11-08 22:09:55 +0900

    add round layout example.

---
+++ Assets/uDesktopDuplication/Examples/Scripts/MultipleMonitorLayouter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MultipleMonitorCreator))]
+public class MultipleMonitorLayouter : MonoBehaviour
+{
+    protected MultipleMonitorCreator creator_;
+    [SerializeField] bool updateEveryFrame = true;
+    [SerializeField] protected float margin = 0.1f;
+
+    void Start()
+    {
+        creator_ = GetComponent<MultipleMonitorCreator>();
+        Layout();
+    }
+
+    protected virtual void Update()
+    {
+        if (updateEveryFrame) {
+            Layout();
+        }
+    }
+
+    void LateUpdate()
+    {
+        margin = Mathf.Max(margin, 0f);
+    }
+
+    protected virtual void Layout()
+    {
+        var monitors = creator_.monitors;
+        var n = monitors.Count;
+
+        var totalWidth = 0f;
+        foreach (var info in monitors) {
+            var width = info.uddTexture.monitor.widthMeter * info.meshBounds.x;
+            totalWidth += width;
+        }
+        totalWidth += margin * (n - 1);
+
+        var x = -totalWidth / 2;
+
+        foreach (var info in creator_.monitors) {
+            var monitor = info.uddTexture.monitor;
+            x += (monitor.widthMeter * info.meshBounds.x) / 2;
+            info.gameObject.transform.localPosition = new Vector3(x, 0f, 0f);
+            info.gameObject.transform.localRotation = info.originalRotation;
+            x += (monitor.widthMeter * info.meshBounds.x) / 2 + margin;
+        }
+    }
+
+}
+
--- Assets/uDesktopDuplication/Shaders/uDD_Unlit.shader
+++ Assets/uDesktopDuplication/Shaders/uDD_Unlit.shader
@@ -5,8 +5,8 @@ Properties
 {
     _Color ("Color", Color) = (1, 1, 1, 1)
     _MainTex ("Texture", 2D) = "white" {}
-    [Toggle(USE_BEND)] _UseBend("UseBend", Float) = 0
-    [PowerSlider(10.0)]_Radius ("Radius", Range(10, 100)) = 30
+    [KeywordEnum(Off, Y, Z)] _Bend("Bending", Int) = 0
+    [PowerSlider(10.0)]_Radius ("Radius", Range(1, 100)) = 30
     [KeywordEnum(Off, Front, Back)] _Cull("Culling", Int) = 2
 }
 
@@ -21,15 +21,20 @@ CGINCLUDE
 
 #include "./uDD_Common.cginc"
 
-fixed _Radius;
+half _Radius;
+half _Width;
 
 v2f vert(appdata v)
 {
     v2f o;
-#ifdef USE_BEND
-    half a = v.vertex.x / _Radius;
-    v.vertex.x =  _Radius * sin(a);
+#if defined(_BEND_Z) || defined(_BEND_Y)
+    half a = _Width * v.vertex.x / _Radius;
+    v.vertex.x = _Radius * sin(a) / _Width;
+    #ifdef _BEND_Y
     v.vertex.y += _Radius * (1 - cos(a));
+    #elif _BEND_Z
+    v.vertex.z += _Radius * (1 - cos(a));
+    #endif
 #endif
     o.vertex = UnityObjectToClipPos(v.vertex);
     o.uv = TRANSFORM_TEX(v.uv, _MainTex);
@@ -51,8 +56,8 @@ Pass
     #pragma multi_compile ___ INVERT_X
     #pragma multi_compile ___ INVERT_Y
     #pragma multi_compile ___ ROTATE90 ROTATE180 ROTATE270
-    #pragma multi_compile ___ USE_BEND
     #pragma multi_compile ___ USE_CLIP
+    #pragma multi_compile _BEND_OFF _BEND_Y _BEND_Z
     ENDCG
 }
 
