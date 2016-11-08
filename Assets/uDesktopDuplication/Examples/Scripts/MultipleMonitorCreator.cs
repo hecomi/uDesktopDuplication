f69fb6a7bb3df7c2d07e4b63d3f8692bab398377
hecomi <info@hecomi.com>
2016-11-09 00:50:57 +0900

    remove monitor if unsupported.

--- Assets/uDesktopDuplication/Examples/Scripts/MultipleMonitorCreator.cs
+++ Assets/uDesktopDuplication/Examples/Scripts/MultipleMonitorCreator.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 public class MultipleMonitorCreator : MonoBehaviour
 {
     [SerializeField] GameObject monitorPrefab;
+    [SerializeField] bool removeIfUnsupported = true;
+    [SerializeField] float removeWaitDuration = 5f;
+    bool hasMonitorUnsupportStateChecked = false;
+    float removeWaitTimer_ = 0f;
 
     public class MonitorInfo
     {
@@ -13,12 +17,19 @@ public class MultipleMonitorCreator : MonoBehaviour
         public Vector3 meshBounds;
     }
 
-   private List<MonitorInfo> monitors_ = new List<MonitorInfo>();
-   public List<MonitorInfo> monitors { get { return monitors_; } }
+    private List<MonitorInfo> monitors_ = new List<MonitorInfo>();
+    public List<MonitorInfo> monitors { get { return monitors_; } }
 
     void Start()
     {
         Create();
+    }
+
+    void Update()
+    {
+        if (removeIfUnsupported) {
+            RemoveUnsupportedDisplayAfterRemoveTimer();
+        }
     }
 
     void OnEnable()
@@ -31,14 +42,37 @@ public class MultipleMonitorCreator : MonoBehaviour
         uDesktopDuplication.Manager.onReinitialized -= Recreate;
     }
 
+    void RemoveUnsupportedDisplayAfterRemoveTimer()
+    {
+        if (!hasMonitorUnsupportStateChecked) {
+            removeWaitTimer_ += Time.deltaTime;
+            if (removeWaitTimer_ > removeWaitDuration) {
+                hasMonitorUnsupportStateChecked = true;
+                foreach (var info in monitors_) {
+                    if (info.uddTexture.monitor.state == uDesktopDuplication.MonitorState.Unsupported) {
+                        Destroy(info.gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    void ResetRemoveTimer()
+    {
+        hasMonitorUnsupportStateChecked = false;
+        removeWaitTimer_ = 0f;
+    }
+
     void Create()
     {
+        ResetRemoveTimer();
+
         // Sort monitors in coordinate order
         var monitors = uDesktopDuplication.Manager.monitors;
         monitors.Sort((a, b) => a.left - b.left);
 
         // Create monitors
-        for (int i = 0 ; i < uDesktopDuplication.Manager.monitorCount; ++i) {
+        for (int i = 0; i < uDesktopDuplication.Manager.monitorCount; ++i) {
             // Create monitor obeject
             var go = Instantiate(monitorPrefab);
             go.name = "Monitor " + i;
