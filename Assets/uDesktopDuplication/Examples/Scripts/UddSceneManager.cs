74e25f2bd796afcf3c060f706502ddcaea630e40
hecomi <info@hecomi.com>
2016-11-12 14:38:13 +0900

    add scene manager.

---
+++ Assets/uDesktopDuplication/Examples/Scripts/UddSceneManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UddSceneManager : MonoBehaviour
+{
+    public static UddSceneManager instance { get; set; }
+    [SerializeField] string[] scenes;
+    [SerializeField] int sceneNo = 0;
+
+    void Awake()
+    {
+        if (!instance) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Load();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Next();
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Prev();
+        }
+    }
+
+    void Next()
+    {
+        sceneNo = (sceneNo + 1) % scenes.Length;
+        Load();
+    }
+
+    void Prev()
+    {
+        sceneNo = (sceneNo - 1) % scenes.Length;
+        Load();
+    }
+
+    void Load()
+    {
+        SceneManager.LoadScene(scenes[sceneNo]);
+    }
+}
