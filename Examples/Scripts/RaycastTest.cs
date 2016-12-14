247a92caf8c2c5e0d654a0200a2fcd3e86d6ea3a
hecomi <info@hecomi.com>
2016-12-15 02:30:04 +0900

    add raycast api.

---
+++ Examples/Scripts/RaycastTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastTest : MonoBehaviour
+{
+    public Transform from;
+    public Transform to;
+
+    void Update()
+    {
+        if (!from || !to) return;
+
+        Debug.DrawLine(from.position, to.position, Color.red);
+
+        foreach (var uddTexture in GameObject.FindObjectsOfType<uDesktopDuplication.Texture>()) {
+            var result = uddTexture.RayCast(from.position, to.position - from.position);
+            if (result.hit) {
+                Debug.DrawLine(result.position, result.position + result.normal, Color.yellow);
+            }
+        }
+    }
+}
